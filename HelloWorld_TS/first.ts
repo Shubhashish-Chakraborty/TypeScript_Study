c76543d58cd720fc82465a039361d83e5ad027ca
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-05-08 23:49:19 +0530

    Getting Started - Initial Commit

---
+++ HelloWorld_TS/first.ts
@@ -0,0 +1,12 @@
+
+
+let data = {
+    name: "Shubhashish",
+    age: 12,
+    language: "TypeScript"   
+}
+
+console.log("Hello world, Shubhashish!!, My First Typescript Code!!");
+console.log(data.language);
+
+// Run : tsc filename.ts => will compile this ts file to js code!!
