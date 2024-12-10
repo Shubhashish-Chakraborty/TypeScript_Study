77ce184ea0fafb9d1de8d3c956dc595bbefcd6e2
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-12-10 14:00:08 +0530

    directories distributions

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Write a function which itself takes a function and run it after 1 second
-function delayedCall(fnToDelay) {
-    setTimeout(fnToDelay, 1000);
-}
-delayedCall(() => {
-    console.log("Hello to the world after 1 second !");
-});
